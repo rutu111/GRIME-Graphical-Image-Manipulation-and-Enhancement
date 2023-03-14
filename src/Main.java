20084af3097fb9a3da58b7646778c0037c05c5fb
Srijha09 <srijha.1999@gmail.com>
2023-03-14 13:18:44 -0400

    updated main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 public class Main {
-  public Main() {
-  }
-
   public static void main(String[] args) {
     Model model = new Model();
     ImageController controller = new ImageController(model);
