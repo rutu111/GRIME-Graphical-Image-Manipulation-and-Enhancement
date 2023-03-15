4c1217c6e1f36c2ecf39e3488b7c7147da84f6ac
Srijha09 <srijha.1999@gmail.com>
2023-03-15 18:59:43 -0400

    added more controller test cases

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,12 @@ import Controller.ImageController;
 import Model.Model;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import Model.Operations;
 
 public class Main {
   public static void main(String[] args) throws IOException {
 
-    Model model = new Model();
+    Operations model = new Model();
     ImageController controller = new ImageController(model, new InputStreamReader(System.in),System.out);
     controller.run();
   }
--- test/ImageControllerTest.java
+++ test/ImageControllerTest.java
@@ -11,12 +11,17 @@ import java.io.StringReader;
 import java.util.NoSuchElementException;
 import org.junit.Test;
 
-/**'
- * This is a test class to test the controller in isolation
+/**
+ * ' This is a test class to test the controller in isolation.
  */
 public class ImageControllerTest {
 
 
+  /**
+   * This is a MockModel class used to test the controller in isolation that implements the
+   * Operations interface from the model. The mockmodel is used to make sure the controller
+   * transcends inputs to the model and receives the outputs from the model.
+   */
   public class MockModel implements Operations {
 
     public StringBuilder log;
@@ -105,37 +110,191 @@ public class ImageControllerTest {
 
   }
 
+  //LOAD
   @Test
-  public void testHorizontalFlip() throws Exception {
+  public void testLoad() throws Exception {
     StringBuffer out = new StringBuffer();
-    String imageName = "test-image";
-    String newImageName = "updated-image";
-    Reader in = new StringReader("horizontal-flip koala koala-horizontal\nexit");
-    StringBuilder log = new StringBuilder(); //log for mock model
-    MockModel mockModel = new MockModel(log);
-    ImageController imageController = new ImageController(mockModel, in, out);
-    imageController.run();
-    mockModel.horizontalFlip(imageName, newImageName);
-    String expectedOutput =
-        "Received inputs for createBuilderThreeChannel: " + 1024 + " " + 768 + "\n"
-            + "Received inputs for horizontalFlip: " + imageName + " " + newImageName + "\n";
-    assertEquals(expectedOutput, mockModel.log.toString()); //inputs reached the model correctly
-  }
-
-  @Test
-  public void testVerticalFlip() throws Exception {
-    StringBuffer out = new StringBuffer();
-    String imageName = "test-image";
-    String newImageName = "updated-image";
+    String imageName = "koala";
+    int width = 1024;
+    int height = 768;
     Reader in = new StringReader("load koala.ppm koala\nexit");
     StringBuilder log = new StringBuilder(); //log for mock model
     MockModel mockModel = new MockModel(log);
     ImageController imageController = new ImageController(mockModel, in, out);
     imageController.run();
-    mockModel.verticalFlip(imageName, newImageName);
     String expectedOutput =
-        "Received inputs for createBuilderThreeChannel: " + 1024 + " " + 768 + "\n"
-            + "Received inputs for verticalFlip: " + imageName + " " + newImageName + "\n";
+        "Received inputs for createBuilderThreeChannel: " + width + " " + height + "\n";
+    assertEquals(expectedOutput, mockModel.log.toString()); //inputs reached the model correctly
+  }
+
+  //should not give an output
+  @Test
+  public void testLoadNonPPMFile() throws Exception {
+    StringBuffer out = new StringBuffer();
+    String imageName = "koala";
+    Reader in = new StringReader("load koala.jpg koala\nexit");
+    StringBuilder log = new StringBuilder(); //log for mock model
+    MockModel mockModel = new MockModel(log);
+    ImageController imageController = new ImageController(mockModel, in, out);
+    imageController.run();
+    String expectedOutput = "";
+    assertEquals(expectedOutput, mockModel.log.toString()); //inputs reached the model correctly
+  }
+
+
+  //VERTICAL FLIP
+  @Test
+  public void testVerticalFlip() throws Exception {
+    StringBuffer out = new StringBuffer();
+    String imageName = "koala";
+    String newImageName = "koala-vertical";
+    Reader in = new StringReader("vertical-flip koala koala-vertical\nexit");
+    StringBuilder log = new StringBuilder(); //log for mock model
+    MockModel mockModel = new MockModel(log);
+    ImageController imageController = new ImageController(mockModel, in, out);
+    imageController.run();
+    String expectedOutput =
+        "Received inputs for verticalFlip: " + imageName + " " + newImageName + "\n";
+    assertEquals(expectedOutput, mockModel.log.toString()); //inputs reached the model correctly
+  }
+
+  //HORIZONTAL FLIP
+  @Test
+  public void testHorizontalFlip() throws Exception {
+    StringBuffer out = new StringBuffer();
+    String imageName = "koala";
+    String newImageName = "koala-horizontal";
+    Reader in = new StringReader("horizontal-flip koala koala-horizontal\nexit");
+    StringBuilder log = new StringBuilder(); //log for mock model
+    MockModel mockModel = new MockModel(log);
+    ImageController imageController = new ImageController(mockModel, in, out);
+    imageController.run();
+    String expectedOutput =
+        "Received inputs for horizontalFlip: " + imageName + " " + newImageName + "\n";
+    assertEquals(expectedOutput, mockModel.log.toString()); //inputs reached the model correctly
+  }
+
+
+  //BRIGHTEN
+  @Test
+  public void testBrighten() throws Exception {
+    StringBuffer out = new StringBuffer();
+    String imageName = "koala";
+    String newImageName = "koala-brighten";
+    double increment = 10;
+    Reader in = new StringReader("brighten 10 koala koala-brighten\nexit");
+    StringBuilder log = new StringBuilder(); //log for mock model
+    MockModel mockModel = new MockModel(log);
+    ImageController imageController = new ImageController(mockModel, in, out);
+    imageController.run();
+    String expectedOutput =
+        "Received inputs for brighten: " + imageName + " " + newImageName + " " + increment +"\n";
+    assertEquals(expectedOutput, mockModel.log.toString()); //inputs reached the model correctly
+  }
+
+  //GREYSCALE VISUALIZE:value
+  @Test
+  public void testGreyScaleValue() throws Exception {
+    StringBuffer out = new StringBuffer();
+    String imageName = "koala";
+    String newImageName = "koala-greyscale-value";
+    String measure = "value";
+    Reader in = new StringReader("greyscale value-component koala koala-greyscale-value\nexit");
+    StringBuilder log = new StringBuilder(); //log for mock model
+    MockModel mockModel = new MockModel(log);
+    ImageController imageController = new ImageController(mockModel, in, out);
+    imageController.run();
+    String expectedOutput =
+        "Received inputs for visualizeValueIntensityLuma: " + imageName + " " + newImageName + " "
+            + measure + "\n";
+    assertEquals(expectedOutput, mockModel.log.toString()); //inputs reached the model correctly
+  }
+
+  //GREYSCALE VISUALIZE:luma
+  @Test
+  public void testGreyScaleLuma() throws Exception {
+    StringBuffer out = new StringBuffer();
+    String imageName = "koala";
+    String newImageName = "koala-greyscale-luma";
+    String measure = "luma";
+    Reader in = new StringReader("greyscale luma-component koala koala-greyscale-luma\nexit");
+    StringBuilder log = new StringBuilder(); //log for mock model
+    MockModel mockModel = new MockModel(log);
+    ImageController imageController = new ImageController(mockModel, in, out);
+    imageController.run();
+    String expectedOutput =
+        "Received inputs for visualizeValueIntensityLuma: " + imageName + " " + newImageName + " "
+            + measure + "\n";
+    assertEquals(expectedOutput, mockModel.log.toString()); //inputs reached the model correctly
+  }
+
+  //GREYSCALE VISUALIZE:intensity
+  @Test
+  public void testGreyScaleIntensity() throws Exception {
+    StringBuffer out = new StringBuffer();
+    String imageName = "koala";
+    String newImageName = "koala-greyscale-intensity";
+    String measure = "intensity";
+    Reader in = new StringReader("greyscale intensity-component koala koala-greyscale-intensity\nexit");
+    StringBuilder log = new StringBuilder(); //log for mock model
+    MockModel mockModel = new MockModel(log);
+    ImageController imageController = new ImageController(mockModel, in, out);
+    imageController.run();
+    String expectedOutput =
+        "Received inputs for visualizeValueIntensityLuma: " + imageName + " " + newImageName + " "
+            + measure + "\n";
+    assertEquals(expectedOutput, mockModel.log.toString()); //inputs reached the model correctly
+  }
+
+  //GREYSCALE VISUALIZE:Red
+  @Test
+  public void testGreyScaleRed() throws Exception {
+    StringBuffer out = new StringBuffer();
+    String imageName = "koala";
+    String newImageName = "koala-greyscale-red";
+    String component = "red";
+    Reader in = new StringReader("greyscale red-component koala koala-greyscale-red\nexit");
+    StringBuilder log = new StringBuilder(); //log for mock model
+    MockModel mockModel = new MockModel(log);
+    ImageController imageController = new ImageController(mockModel, in, out);
+    imageController.run();
+    String expectedOutput =
+        "Received inputs for visIndividualComponent: " + imageName + " " + newImageName + " "
+            + component + "\n";
+    assertEquals(expectedOutput, mockModel.log.toString()); //inputs reached the model correctly
+  }
+
+  @Test
+  public void testGreyScaleGreen() throws Exception {
+    StringBuffer out = new StringBuffer();
+    String imageName = "koala";
+    String newImageName = "koala-greyscale-green";
+    String component = "green";
+    Reader in = new StringReader("greyscale green-component koala koala-greyscale-green\nexit");
+    StringBuilder log = new StringBuilder(); //log for mock model
+    MockModel mockModel = new MockModel(log);
+    ImageController imageController = new ImageController(mockModel, in, out);
+    imageController.run();
+    String expectedOutput =
+        "Received inputs for visIndividualComponent: " + imageName + " " + newImageName + " "
+            + component + "\n";
+    assertEquals(expectedOutput, mockModel.log.toString()); //inputs reached the model correctly
+  }
+
+  @Test
+  public void testGreyScaleBlue() throws Exception {
+    StringBuffer out = new StringBuffer();
+    String imageName = "koala";
+    String newImageName = "koala-greyscale-blue";
+    String component = "blue";
+    Reader in = new StringReader("greyscale blue-component koala koala-greyscale-blue\nexit");
+    StringBuilder log = new StringBuilder(); //log for mock model
+    MockModel mockModel = new MockModel(log);
+    ImageController imageController = new ImageController(mockModel, in, out);
+    imageController.run();
+    String expectedOutput =
+        "Received inputs for visIndividualComponent: " + imageName + " " + newImageName + " "
+            + component + "\n";
     assertEquals(expectedOutput, mockModel.log.toString()); //inputs reached the model correctly
   }
 }
