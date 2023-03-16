d544a415ac2633c719c5d0eab9880e757a40c6f0
Srijha09 <srijha.1999@gmail.com>
2023-03-16 00:05:17 -0400

    added more controller test cases

---
+++ test/ImageControllerModelTest.java
@@ -0,0 +1,9 @@
+/**
+ * This is a class that tests the Model and controller together to make sure the controller is able
+ * to get the objects created in the model and performs necessary work to send it to the view.
+ */
+public class ImageControllerModelTest {
+
+
+
+}
